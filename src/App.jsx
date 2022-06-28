80cde6be020f524a425e35eea8806254f8e44d02
Sidneycarlinijr <sidney_carlini@hotmail.com>
2022-06-28 09:30:18 -0300

    Finalizado estrutura da calculadora (grid)

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,25 @@ export default function App() {
   return (
     <div className="box bg-white">
       <Display></Display>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
+
+      <Button>CE</Button>
+      <Button>/</Button>
+      <Button>*</Button>
+      <Button>-</Button>
+      <Button>7</Button>
+      <Button>8</Button>
+      <Button>9</Button>
+      <Button>+</Button>
+      <Button>4</Button>
+      <Button>5</Button>
+      <Button>6</Button>
+      <Button>-</Button>
+      <Button>1</Button>
+      <Button>2</Button>
+      <Button>3</Button>
+      <Button row={1}>=</Button>
+      <Button span={2}>0</Button>
+      <Button>,</Button>
     </div>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,6 +4,12 @@ import './button.css'
 export default function (props) {
 
     return (
-        <button className="defaultButton mx-0">N</button>
+        <button 
+            className={`defaultButton ${ props.span != null ? `col-span-${props.span}` : ''}
+                ${props.row != null ? `row-span-2` : ''}`
+        }
+            
+            >{props.children}
+        </button>
     )
 }
