52901fbddbbb35c96a3557eb89060f049bfea3e4
Sidneycarlinijr <sidney_carlini@hotmail.com>
2022-07-04 10:20:57 -0300

    Neste momento, por meio da comunicacao indireta, todos os botoes estao passando seu value/children para o parent

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,12 +4,12 @@ import './button.css'
 export default function (props) {
     return (
         <button
+            onClick={e => props.click(props.children)}
             className={`defaultButton border-r border-b border-gray-100 hover:bg-gray-300
-                ${props.span != null ? `col-span-2` : ''}
-                ${props.row != null ? `row-span-2` : ''}
-                ${props.bgColor != null ? `bg-${props.bgColor} hover:bg-blue-400` : 'bg-gray-100'}`
+                ${props.span ? `col-span-2` : ''}
+                ${props.row ? `row-span-2` : ''}
+                ${props.bgColor ? `bg-${props.bgColor} hover:bg-blue-400` : 'bg-gray-100'}`
             }
-
         >{props.children}
         </button>
     )
--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -4,27 +4,31 @@ import Display from './Display'
 
 export default function Calculator() {
 
+    function setNumber(e) {
+        console.log(e)
+    }
+
     return (
         <div className="box rounded">
             <Display></Display>
 
-            <Button bgColor="blue-100">CE</Button>
-            <Button >/</Button>
-            <Button >*</Button>
-            <Button >-</Button>
-            <Button>7</Button>
-            <Button>8</Button>
-            <Button>9</Button>
-            <Button row='2'>+</Button>
-            <Button>4</Button>
-            <Button>5</Button>
-            <Button>6</Button>
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
-            <Button bgColor="blue-100" row='2'>=</Button>
-            <Button span='2'>0</Button>
-            <Button>,</Button>
+            <Button click={setNumber} bgColor="blue-100">CE</Button>
+            <Button click={setNumber}  >/</Button>
+            <Button click={setNumber}  >*</Button>
+            <Button click={setNumber}  >-</Button>
+            <Button click={setNumber} >7</Button>
+            <Button click={setNumber} >8</Button>
+            <Button click={setNumber} >9</Button>
+            <Button click={setNumber} row='2'>+</Button>
+            <Button click={setNumber} >4</Button>
+            <Button click={setNumber} >5</Button>
+            <Button click={setNumber} >6</Button>
+            <Button click={setNumber} >1</Button>
+            <Button click={setNumber} >2</Button>
+            <Button click={setNumber} >3</Button>
+            <Button click={setNumber} bgColor='blue-100' row='2'>=</Button>
+            <Button click={setNumber} span='2'>0</Button>
+            <Button click={setNumber} >,</Button>
         </div>
     )
 }
