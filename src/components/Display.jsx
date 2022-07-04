094025116f3e2d4e31ec7336b73aede492fda3a4
Sidneycarlinijr <sidney_carlini@hotmail.com>
2022-07-04 11:19:05 -0300

    inicio da renderizacao dos numeros no display

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-export default function (props){
-    return(
-    <div className="bg-blue-200 col-span-4 text-white text-3xl flex items-end justify-end">
-        0
-    </div>
+export default function (props) {
+
+    return (
+        <div className="bg-blue-200 col-span-4 text-white text-3xl flex items-end justify-end">
+            {props.num}
+        </div>
     )
 }
